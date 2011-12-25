daacd07b66e9fc5b38beb53b86c6091a60bf7b70
A J <avjmail@gmail.com>
2011-12-25 14:57:20 +0200

    deletion

--- rails/learn_rails_by_example/first_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '3a15ee0c5067de9355ed080a4e57c66d57f0130beaf05f01c02e676cfc965319fd647d239ff40f876bc53e60069d5b941c8b5e0f1ae318be999a93eb07649459'
